ad00489a4cebfc02b61b220dfab0653c9335e150
prabukiwi1 <39737708+prabukiwi1@users.noreply.github.com>
2022-02-08 09:47:22 +0530

    Single Page Application

---
+++ SPA/spa.js
@@ -0,0 +1,47 @@
+angular.module('myapp',['ngRoute'])
+.config(function($routeProvider)
+{
+    $routeProvider.when('/home',
+    {
+        templateUrl:'christ/home.html',
+        controller:'homectrl'}).when('/home/:first/:last',
+        {
+            templateUrl:'christ/home.html',
+            controller:'homectrl'
+    }).when('/course',
+    {
+        templateUrl:'christ/course.html',
+        controller:'coursectrl'
+    }).when('/student',
+    {
+        templateUrl:'christ/student.html',
+        controller:'studentctrl'
+    })
+})
+.controller('myctrl',function()
+{
+
+})
+.controller("homectrl",function($scope,$routeParams)
+{
+    $scope.message="Home Page"
+    if($routeParams.first&&$routeParams.last)
+    {
+        $scope.person={
+            first:$routeParams.first,
+            last:$routeParams.last
+        };
+    }
+})
+.controller("coursectrl",function($scope)
+{
+   $scope.courses=["MCA","MSC","MBA","BCA","BCom","BBA"]; 
+})
+.controller("studentctrl",function($scope,$http)
+{
+    $http.get('student.json')
+    .success(function(response)
+    {
+        $scope.students=response.records;
+    });
+});
